6cf7bdd05f3881cb2d8a0c7384ada07dddfb67b7
Kyle McKean <mckean.kylej@gmail.com>
2016-07-19 10:55:15 -0400

    optparse work

--- src/Stack/Nix/Main.hs
+++ src/Stack/Nix/Main.hs
@@ -2,27 +2,47 @@ module Stack.Nix.Main where
 
 import Options.Applicative
 
-data Opts = Opts
+data Opts = ProjectRoot {
+  projectP :: String } | Files {
+  stackF :: String,
+  cabalF :: String }
 
-data Program = Version | Normal Opts
+data ExecutionType = Version | Normal {
+  optionsN   :: Opts,
+  modifiersN :: Modifiers }
+
+data Modifiers = Modifiers {
+  outputM :: Maybe String }
+
+data Program = Program ExecutionType Modifiers
 
 version = "v0.1.0"
 
 optsP :: Parser Opts
-optsP = pure Opts
+optsP = projectRootP <|> filesP
+  where projectRootP = ProjectRoot <$> strArgument (metavar "PROJECT-ROOT" <>
+                                                    help "The directory that houses the stack.yaml file and *.cabal file.")
+        filesP = Files <$> strOption (long "stack-yaml" <> short 's' <> metavar "FILE" <>
+                                      help "The projects stack.yaml file.")
+                       <*> strOption (long "cabal" <> short 'c' <> metavar "FILE" <>
+                                      help "The projects cabal file.")
 
-versionP :: Parser Program
+modifiersP :: Parser Modifiers
+modifiersP = Modifiers <$> optional (strOption (long "output" <> short 'o' <> metavar "FILE" <>
+                                                help "Where to write the generated nix configuration."))
+
+versionP :: Parser ExecutionType
 versionP = flag' Version (long "version" <> short 'v' <> help "Prints the version number")
 
-programP :: Parser Program
-programP = versionP <|> (Normal <$> optsP)
+programP :: Parser ExecutionType
+programP = Normal <$> optsP <*> modifiersP <|> versionP
 
 handle :: Opts -> IO ()
 handle _ = return ()
 
 main :: IO ()
 main = execParser (info (helper <*> programP) desc) >>= \case
-  Normal opts -> handle opts
+  Normal opts modifiers -> handle opts
   Version -> putStrLn ("Version: " ++ version)
   where desc = (fullDesc <>
                 progDesc "TODO" <>
